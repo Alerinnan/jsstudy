518411e8bcdb25625d3a2fdaa8dbfd357267d0e7
Alerinnan <107584547+Alerinnan@users.noreply.github.com>
2022-06-17 09:52:53 +0300

    Update study.js

--- study.js
+++ study.js
@@ -12,5 +12,10 @@ var b;
 b = a;
 // Only change code below this line
 var a = 9;
+var myFirstName = "Denis";
+var myLastName = "Chirkov";
+
+
+
 
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
