703b0c8de7576d87cf7148d4fb37fb6a1e323b36
Alerinnan <107584547+Alerinnan@users.noreply.github.com>
2022-06-17 09:54:57 +0300

    Update study.js

--- study.js
+++ study.js
@@ -23,7 +23,15 @@ var c = "I am a";
 a = a + 1;
 b = b + 5;
 c = c + " String!";
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
 
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
 
 
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
